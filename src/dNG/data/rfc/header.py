6651ab460619f04f9509e9e25ca5a2ee97f90d9d
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-21 20:38:59 +0200

    Trivial: Removed file extension from URLs in the file header

--- src/dNG/data/rfc/header.py
+++ src/dNG/data/rfc/header.py
@@ -6,13 +6,13 @@ RFC basics for Python
 Easy to use and RFC compliant methods
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?py;rfc_basics
+https://www.direct-netware.de/redirect?py;rfc_basics
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(rfcBasicsVersion)#
 #echo(__FILEPATH__)#
@@ -31,7 +31,7 @@ Parses RFC 7231 compliant headers.
 :copyright: (C) direct Netware Group - All rights reserved
 :package:   rfc_basics.py
 :since:     v0.1.00
-:license:   http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:   https://www.direct-netware.de/redirect?licenses;mpl2
             Mozilla Public License, v. 2.0
 	"""
 
